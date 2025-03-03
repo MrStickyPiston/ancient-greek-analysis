f50372b3cf33808a5954c996153162a589b29917
sticky <mr.sticky.piston@gmail.com>
2025-03-03 13:20:49 +0100

    grouping to file

--- data/scraping/group_parsed.py
+++ data/scraping/group_parsed.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv('data/raw/ancient_greek_first-declension_nouns.txt_parsed.csv', header=None)
+file = 'data/raw/ancient_greek_first-declension_nouns.txt_parsed.csv'
+
+df = pd.read_csv(file, header=None)
 grouped = df.groupby(0)
 
 unique_groups = {}
 
 for name, group in grouped:
-    important_values = group[[3, 5, 9, 10, 11]].fillna('NULL').values.tolist()
+    important_values = group[[3, 5, 9, 10, 11, 12]].fillna('NULL').values.tolist()
 
     key = tuple(map(tuple, important_values))
 
@@ -17,7 +19,8 @@ for name, group in grouped:
 
 result = [groups for groups in unique_groups.values() if len(groups) > 1]
 
-for idx, group in enumerate(result):
-    print(f"\nGroup {idx + 1}:")
-    for entry in group:
-        print(entry[0][0], end=" ")
+with open(file.replace('.txt_parsed.csv', '_grouped.txt'), mode='w', newline='') as f:
+    for idx, group in enumerate(result):
+        for entry in group:
+            f.write(entry[0][0] + " ")
+        f.write("\n")
