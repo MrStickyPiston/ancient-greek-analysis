df001573778c30e2167f2c7082c4fd1d5ddbc882
sticky <mr.sticky.piston@gmail.com>
2025-03-03 12:24:21 +0100

    use panda dataframe for group parsing/filtering

--- data/scraping/group_parsed.py
+++ data/scraping/group_parsed.py
@@ -1,72 +1,23 @@
-import csv
-from collections import defaultdict
+import pandas as pd
 
-class UnionFind:
-    def __init__(self):
-        self.parent = {}
-        self.rank = {}
+df = pd.read_csv('data/raw/ancient_greek_first-declension_nouns.txt_parsed.csv', header=None)
+grouped = df.groupby(0)
 
-    def find(self, item):
-        if self.parent[item] != item:
-            self.parent[item] = self.find(self.parent[item])  # Path compression
-        return self.parent[item]
+unique_groups = {}
 
-    def union(self, item1, item2):
-        root1 = self.find(item1)
-        root2 = self.find(item2)
+for name, group in grouped:
+    important_values = group[[3, 5, 9, 10, 11]].fillna('NULL').values.tolist()
 
-        if root1 != root2:
-            # Union by rank
-            if self.rank[root1] > self.rank[root2]:
-                self.parent[root2] = root1
-            elif self.rank[root1] < self.rank[root2]:
-                self.parent[root1] = root2
-            else:
-                self.parent[root2] = root1
-                self.rank[root1] += 1
+    key = tuple(map(tuple, important_values))
 
-    def add(self, item):
-        if item not in self.parent:
-            self.parent[item] = item
-            self.rank[item] = 0
+    if key not in unique_groups:
+        unique_groups[key] = []
 
-# Initialize a defaultdict to store the data
-data_dict = defaultdict(dict)
+    unique_groups[key].append(group.values.tolist())
 
-# Read the CSV data
-with open('data/raw/ancient_greek_first-declension_nouns.txt_parsed.csv', mode='r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        outer_key = row[0]
-        inner_key = (row[-2], row[-3], row[-4])
-        inner_value = row[1:-3]
-        data_dict[outer_key][inner_key] = inner_value
+result = [groups for groups in unique_groups.values() if len(groups) > 1]
 
-# Create a union-find instance
-uf = UnionFind()
-
-# Map to keep track of the indices of each row
-index_map = {}
-
-# Populate the union-find structure
-for index, (outer_key, inner_dict) in enumerate(data_dict.items()):
-    for inner_key in inner_dict.keys():
-        uf.add(index)  # Add the index to the union-find structure
-        index_map[index] = outer_key  # Map index to outer key
-        # Union the current index with others that share the same inner_key
-        for other_index in index_map:
-            if other_index != index and inner_key in data_dict[index_map[other_index]]:
-                uf.union(index, other_index)
-
-# Collect the results
-groups = defaultdict(list)
-for index in range(len(data_dict)):
-    root = uf.find(index)
-    groups[root].append(index_map[index])
-
-# Convert the groups to a list of lists
-result = list(groups.values())
-
-# Print the resulting groups
-for group in result:
-    print(group)
+for idx, group in enumerate(result):
+    print(f"\nGroup {idx + 1}:")
+    for entry in group:
+        print(entry[0][0], end=" ")
