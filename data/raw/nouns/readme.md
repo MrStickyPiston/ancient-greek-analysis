2d2e07a45a248f754a3f68387606c9c53fa6e024
sticky <mr.sticky.piston@gmail.com>
2025-03-08 17:45:56 +0100

    proper nouns

---
+++ data/raw/nouns/readme.md
@@ -0,0 +1,3 @@
+## Exceptions
+https://en.wiktionary.org/wiki/Category:Ancient_Greek_nominalized_adjectives - Different table layout
+https://en.wiktionary.org/wiki/Category:Ancient_Greek_proper_nouns - Own category, own file starting with proper
--- data/scraping/get_nouns.py
+++ data/scraping/get_nouns.py
@@ -3,43 +3,54 @@ import os
 
 API_URL = "https://en.wiktionary.org/w/api.php"
 
-params = {
-    "action": "query",
-    "list": "categorymembers",
-    "cmtitle": "Category:Ancient_Greek_nouns",
-    "cmtype": "page",
-    "cmlimit": "500",
-    "format": "json"
-}
+def get_category(category):
+    params = {
+        "action": "query",
+        "list": "categorymembers",
+        "cmtitle": category,
+        "cmtype": "page",
+        "cmlimit": "500",
+        "format": "json"
+    }
 
-session = requests.Session()
-ids = []
-continue_params = {}
+    session = requests.Session()
+    ids = []
+    continue_params = {}
 
-while True:
-    query_params = params.copy()
-    query_params.update(continue_params)
+    while True:
+        query_params = params.copy()
+        query_params.update(continue_params)
 
-    response = session.get(API_URL, params=query_params)
-    data = response.json()
+        response = session.get(API_URL, params=query_params)
+        data = response.json()
 
-    category_members = data.get("query", {}).get("categorymembers", [])
+        category_members = data.get("query", {}).get("categorymembers", [])
 
-    # Save each pageid as a string
-    for member in category_members:
-        ids.append(str(member["title"]))
+        # Save each pageid as a string
+        for member in category_members:
+            ids.append(str(member["title"]))
 
-    print(f"\rProgress: {len(ids)}", end="")
+        print(f"\rProgress for {category}: {len(ids)}", end="")
 
-    # Check if there is a continuation token for more results
-    if "continue" in data:
-        continue_params = data["continue"]
-    else:
-        break  # No further data
+        # Check if there is a continuation token for more results
+        if "continue" in data:
+            continue_params = data["continue"]
+        else:
+            break  # No further data
+
+    print()
+
+    return ids
 
 folder = "data/raw/nouns/"
 
+ids = get_category("Category:Ancient_Greek_nouns")
 os.makedirs(folder, exist_ok=True)
 with open(folder + "all.txt", 'w') as file:
+    for id in ids:
+        file.write(id + "\n")
+
+ids = get_category("Category:Ancient_Greek_proper_nouns")
+with open(folder + "proper.txt", 'w') as file:
     for id in ids:
         file.write(id + "\n")
